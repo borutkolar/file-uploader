f82229e03b5eade6cbb34c5e4043a2c4cadcaca3
Borut Kolar <borut60@gmail.com>
2025-10-16 09:35:53 +0200

    Implemented the component to show list of files and rendered it conditionally inside file uploader

---
+++ src/client/components/FileUploader/FileList/index.tsx
@@ -0,0 +1,22 @@
+import { type FC } from 'react';
+
+import { type UploadedFile } from '../types';
+
+interface Props {
+    files: UploadedFile[];
+}
+
+const FileList: FC<Props> = ({ files }) => {
+    return (
+        <ul className="w-full">
+            {files.map((file, index) => (
+                <li key={index} className="flex justify-between border-b py-2">
+                    <span>{file.name}</span>
+                    <span>{file.size / 1000} MB</span>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default FileList;
--- src/client/components/FileUploader/index.tsx
+++ src/client/components/FileUploader/index.tsx
@@ -1,45 +1,34 @@
 import { type FC } from 'react';
 
+import FileList from './FileList';
+
 interface Props {
+    onUpload: (files: FileList) => Promise<void>;
     allowMultiple?: boolean;
+    files?: { name: string; size: number }[];
 }
 
-const FileUploader: FC<Props> = ({ allowMultiple }) => {
+const FileUploader: FC<Props> = ({ allowMultiple, files, onUpload }) => {
     const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if (!files) {
             return;
         }
 
-        for (const file of files) {
-            try {
-                const formData = new FormData();
-                formData.append('file', file, file.name);
-
-                const response = await fetch('/api/upload-single', {
-                    method: 'POST',
-                    body: formData,
-                });
-                const data = (await response.json()) as unknown;
-
-                console.log('response', data);
-            } catch (error) {
-                console.error('Error uploading file:', error);
-            } finally {
-                event.target.value = '';
-            }
-        }
+        await onUpload(files);
+        event.target.value = '';
     };
 
     return (
-        <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+        <div className="flex h-full w-full flex-col align items-start">
             <input className="hidden" id="file-upload" type="file" onChange={handleChange} multiple={allowMultiple} />
             <label
-                className="cursor-pointer inline-flex items-center justify-center px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg"
+                className="cursor-pointer inline-flex items-center justify-center mb-4 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg"
                 htmlFor="file-upload"
             >
                 Upload
             </label>
+            {files ? files.length === 0 ? <p>No files uploaded yet.</p> : <FileList files={files} /> : null}
         </div>
     );
 };
